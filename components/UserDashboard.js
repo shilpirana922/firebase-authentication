f8822c0f88c80c2ec7e504396e36bf797836e969
shilpirana922 <ranashilpi922@gmail.com>
2023-04-11 17:08:05 +0530

    Login/Logout code

---
+++ components/UserDashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function UserDashboard() {
+    return (
+        <div className='w-full max-w-[65ch] text-xs sm:text-sm mx-auto flex flex-col flex-1 gap-3 sm:gap-5'>
+            <div className='flex items-stretch'>
+            <h1 className='text-3xl select-none sm:text-6xl'>Hello World</h1>
+            </div>
+        </div>
+    )
+}
